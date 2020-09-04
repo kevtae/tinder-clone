7a6a623f1929c3f23ea2b939ec8ffbb8136aa558
kevtae <kevin0945@gmail.com>
2020-09-04 17:42:07 -0400

    updates

--- src/component/chat.component.jsx
+++ src/component/chat.component.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import "./chat.css";
+import { Link } from "react-router-dom";
 
 const Chat = ({ name, message, profilePic, timestamp }) => {
   return (
-    <div className="chat">
-      <Avatar className="chat__image" alt={name} src={profilePic} />
-      <div className="chat__details">
-        <h2>{name}</h2>
-        <p>{message}</p>
+    <Link to={`/chat/${name}`}>
+      <div className="chat">
+        <Avatar className="chat__image" alt={name} src={profilePic} />
+        <div className="chat__details">
+          <h2>{name}</h2>
+          <p>{message}</p>
+        </div>
+        <div>
+          <p className="chat__timestamp">{timestamp}</p>
+        </div>
       </div>
-      <div>
-        <p className="chat__timestamp">{timestamp}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/component/chatscreen.component.jsx
+++ src/component/chatscreen.component.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import "./chatscreen.css";
+import Avatar from "@material-ui/core/Avatar";
+import { setLogLevel } from "firebase";
+import SelectInput from "@material-ui/core/Select/SelectInput";
 
 const ChatScreen = () => {
-  return <h1>chatscreen</h1>;
+  const [input, setInput] = useState([]);
+  const [messages, setMessages] = useState([
+    {
+      name: "Ellen",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/1200px-Donald_Trump_official_portrait.jpg",
+      message: "Whatsup",
+    },
+    {
+      message: "hey",
+    },
+  ]);
+
+  const handleSend = (e) => {
+    //prevent from refresh
+    e.preventDefault();
+
+    setMessages([...messages, { message: input }]);
+    setInput("");
+  };
+  return (
+    <div className="chatScreen">
+      <p className="chatScreen__timestamp">
+        you matched with ellen on 10/07/20
+      </p>
+      {messages.map((message) =>
+        message.name ? (
+          <div className="chatScreen__message">
+            <Avatar
+              className="chatScreen__image"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
+        ) : (
+          <div className="chatScreen__message">
+            <p className="chatScreen__textuser">{message.message}</p>
+          </div>
+        )
+      )}
+      <div>
+        <form className="chatScreen__input">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="type a message"
+            className="chatScreen__inputField"
+          />
+          <button onClick={handleSend} className="chatScreen__inputButton">
+            send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ChatScreen;
